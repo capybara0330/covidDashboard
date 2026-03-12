4889a22a31a9a0ab194bd4a4425138bf401ad9c9
capybara0330 <lenashea03@gmail.com>
2026-03-12 03:04:01 -0400

    connect components with props and lift state up

--- src/components/display-statistics.tsx
+++ src/components/display-statistics.tsx
@@ -6,12 +6,16 @@ import { fetchCovidData } from "@/services/disease";
 
 const STATISTICS: (keyof CovidData)[] = ["confirmed", "active", "recovered"];
 
-const DisplayStatistics = () => {
+type DisplayStatisticsProps = {
+    countryCode: string;
+};
+
+const DisplayStatistics = ({countryCode}: DisplayStatisticsProps) => {
     const [covidData, setCovidData] = useState<CovidData | null>(null);
 
     useEffect(() => {
-        fetchCovidData("US").then((data) => setCovidData(data));
-    }, []);
+        fetchCovidData(countryCode).then((data) => setCovidData(data));
+    }, [countryCode]); //refetch whenever countrycode changes
 
   return (
     <div className="flex w-full justify-between flex-col sm:flex-row gap-5">
